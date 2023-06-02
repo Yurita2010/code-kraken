a87dba0300353368ad722ee4c3e2fa2c6c47760e
Yurita2010 <123651093+Yurita2010@users.noreply.github.com>
2023-06-02 15:11:12 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -9,6 +9,39 @@ if (code == 0) {
         `)
 }
 basic.forever(function () {
+    if (code == 1 && (input.buttonIsPressed(Button.A) || input.logoIsPressed())) {
+        basic.pause(200)
+        code = 9
+    }
+    if (code == 2 && (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+        basic.pause(200)
+        code = 9
+    }
+    if (code == 3 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
+        basic.pause(200)
+        code = 9
+    }
+    if (code == 4 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
+        basic.pause(200)
+        code = 9
+    }
+    if (code == 5 && (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+        basic.pause(200)
+        code = 9
+    }
+})
+basic.forever(function () {
+    if (code == 9) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(1000)
+        code = 0
+    }
     if (code == 1) {
         basic.showLeds(`
             # . . . .
@@ -47,65 +80,33 @@ basic.forever(function () {
     }
     if (code == 5) {
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
             `)
     }
 })
 basic.forever(function () {
     if (code == 0 && input.buttonIsPressed(Button.A)) {
+        basic.pause(200)
         code = 1
     }
     if (code == 1 && input.buttonIsPressed(Button.B)) {
+        basic.pause(200)
         code = 2
     }
     if (code == 2 && input.logoIsPressed()) {
+        basic.pause(200)
         code = 3
     }
     if (code == 3 && input.buttonIsPressed(Button.A)) {
+        basic.pause(200)
         code = 4
     }
     if (code == 4 && input.logoIsPressed()) {
+        basic.pause(200)
         code = 5
-    }
-    if (code == 1 && (input.buttonIsPressed(Button.A) || input.logoIsPressed())) {
-        code = 9
-    }
-    if (code == 2 && (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
-        code = 9
-    }
-    if (code == 3 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
-        code = 9
-    }
-    if (code == 4 && (input.logoIsPressed() || input.buttonIsPressed(Button.B))) {
-        code = 9
-    }
-    if (code == 5 && (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
-        code = 9
-    }
-})
-basic.forever(function () {
-    if (code == 9) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.pause(1000)
-        code = 0
-    }
-    if (code == 5) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-            `)
     }
 })
